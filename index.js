f800b141c4c358f975f2169df0ad04e234d3bc8d
deehood <dee.hood@gmail.com>
2022-04-12 11:13:49 +0100

    4.2 Blog list, step2

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const config = require("./utils/config");
 const http = require("http");
 const logger = require("./utils/logger");
 
-// app.use(express.static("build"));
-
-const server = http.createServer(app);
+// express cant handle https and http can add optional parameters to the server
+http.createServer(app);
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`);
