11fb657f50007bbbcd13b6368ea8101bef257e4b
deehood <dee.hood@gmail.com>
2022-04-12 13:46:30 +0100

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,18 @@
 const dummy = (blogs) => {
-    // ...
     return 1;
+};
+
+const totaLikes = (blogs) => {
+    if (blogs.length === 0) return 0;
+    if (blogs.length === 1) return blogs[0].likes;
+
+    const total = blogs.reduce((sum, blog) => {
+        return sum + blog.likes;
+    }, 0);
+    return total;
 };
 
 module.exports = {
     dummy,
+    totaLikes,
 };
